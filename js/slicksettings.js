3cef975c36cb6c93b8121be57c3d253cbc0deacc
Oskar <ostrowskioskar@gmail.com>
2022-02-22 14:48:56 +0100

    adding slickjs

---
+++ js/slicksettings.js
@@ -0,0 +1,22 @@
+$('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    mobileFirst: true,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+      ]
+  });
+  
